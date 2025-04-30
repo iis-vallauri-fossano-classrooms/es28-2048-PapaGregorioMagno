63595f81592831d6731ac15c5716b67ff6cecee5
PapaGregorioMagno <g.borbone.3494@vallauri.edu>
2025-04-30 11:38:34 +0200

    consegna

--- index.js
+++ index.js
@@ -3,7 +3,29 @@
 const DIM = 4;
 
 window.onload=function(){
+   let rand = generaNumero();
+   let rand1 = generaNumero();
 
+   creaMat(rand,rand1);
+   
+}
+
+function creaMat(rand,rand1){
+   for(let i = 0;i < DIM; i++)
+   {
+      for(let j = 0; j < DIM; j++)
+      {
+         const div = document.createElement("div");
+         div.classList.add("cella");
+         if (i === rand && j === rand1) {
+            div.textContent = "2";
+        }
+         
+
+
+         wrapper.appendChild(div);
+      }
+   }
 }
 
 function generaNumero(min, max){
